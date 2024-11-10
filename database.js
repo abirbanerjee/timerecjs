47c5b9f5c03ae3ad97cb893aa997b39bd667e3dd
Abir Banerjee <abir.banerjee@gmail.com>
2024-11-10 15:11:24 +0100

    db rename

--- database.js
+++ database.js
@@ -1,5 +1,5 @@
 import sqlite3 from "sqlite3";
-const db = new sqlite3.Database("projects.db");
+const db = new sqlite3.Database("projects");
 
 export default {
   getProjectDetails: async function getProjectDetails(companyName, monthYear) {
